71a68a5cefa281eea1f827b1662ab5e1d401870d
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-05 17:07:13 -0500

    Segundo-proyecto-inyección-dependencias-autowired
    En esta oportunidad estamos inyectando dependencias usando la anotación @AutoWired

--- spring-boot-inyeccion-dependencias/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ spring-boot-inyeccion-dependencias/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
 import com.bolsadeideas.springboot.di.app.services.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    private MiServicio servicio = new MiServicio();
+    @Autowired
+    private MiServicio servicio;
+    
     @GetMapping({"/index", "/", ""})
     public String index(Model model){
         model.addAttribute("titulo", "Probando los servicios sin inyección de dependencias");
--- spring-boot-inyeccion-dependencias/src/main/java/com/bolsadeideas/springboot/di/app/services/MiServicio.java
+++ spring-boot-inyeccion-dependencias/src/main/java/com/bolsadeideas/springboot/di/app/services/MiServicio.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.di.app.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MiServicio {
     public String operacion(){
         return "ejecutando algún proceso importante...";
