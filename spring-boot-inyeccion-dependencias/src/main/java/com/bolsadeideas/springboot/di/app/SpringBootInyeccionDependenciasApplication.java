96ecfc4e83aaeb79c3cc3b01ab5f07c5e9534cff
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-05 15:33:50 -0500

    Segundo-proyecto-inyección-dependencias-inicio
    En esta segunda entrega estaremos viendo la inyección de dependencias

---
+++ spring-boot-inyeccion-dependencias/src/main/java/com/bolsadeideas/springboot/di/app/SpringBootInyeccionDependenciasApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInyeccionDependenciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInyeccionDependenciasApplication.class, args);
+    }
+
+}
