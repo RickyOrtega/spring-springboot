5fd7556022249b7982c67df485f26b0a81a9b4dc
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-11 18:07:39 -0500

    Tercer-proyecto-form-validation-valid
    En esta oportunidad estamos aplicando al controlador la anotación @Valid, la cual relaciona atributos que deben ser válidos.

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,10 +1,16 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 public class FormController {
@@ -15,10 +21,23 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+
+        model.addAttribute("titulo", "Resultado form");
+
+        if(result.hasErrors()){
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(e -> {
+                errores.put(e.getField(), "El campo "
+                        .concat(e.getField())
+                        .concat(" ")
+                        .concat(Objects.requireNonNull(e.getDefaultMessage())));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
 
         model.addAttribute("usuario", usuario);
-        model.addAttribute("titulo", "Resultado form");
 
         return "resultado";
     }
--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,8 +1,14 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Usuario {
+
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 
     public Usuario() {
