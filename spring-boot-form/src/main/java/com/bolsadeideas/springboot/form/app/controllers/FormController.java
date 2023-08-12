8da7e05508b9a28c1eed5b62dd109c3338721104
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-12 03:57:26 -0500

    Tercer-proyecto-form-validation-sessionestatus
    Se realizaron algunos cambios al código, para permitir que persistan los objetos en la sesión

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -6,42 +6,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
     @GetMapping("/form")
     public String form(Model model){
-        Object usuario = new Usuario();
+        Usuario usuario = new Usuario();
+        usuario.setNombre("John");
+        usuario.setApellido("Doe");
+        usuario.setIdentificador("123.456.789-K");
         model.addAttribute("titulo", "Formulario usuarios");
-        model.addAttribute("user", usuario);
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus sessionStatus) {
 
         model.addAttribute("titulo", "Resultado form");
 
         if(result.hasErrors()){
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(e -> {
-                errores.put(e.getField(), "El campo "
-                        .concat(e.getField())
-                        .concat(" ")
-                        .concat(Objects.requireNonNull(e.getDefaultMessage())));
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
 
         model.addAttribute("usuario", usuario);
-
+        sessionStatus.setComplete();
         return "resultado";
     }
 }
--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -3,7 +3,12 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 import jakarta.validation.constraints.NotEmpty;
 
 public class Usuario {
+    private String identificador;
 
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
     @NotEmpty
     private String username;
     @NotEmpty
@@ -42,5 +47,29 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
     }
 }
