26232d1f88d9b3ec158e91ae50b0a3fea8eb9433
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-24 13:13:46 -0500

    Cuarto-proyecto-horario-inicio-sesion-terminado
    Funcionando completamente y arreglada la exception NullPointerException

--- spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/controllers/AppController.java
+++ spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/controllers/AppController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.horariointerceptor.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
+    @Value("${config.horario.apertura}")
+    private Integer apertura;
+    @Value("${config.horario.cierre}")
+    private Integer cierre;
+
     @GetMapping({"/", "/index"})
     public String index(Model model){
         model.addAttribute("titulo", "Bienvenido al horario de atención a cliente");
-        return "index";
+        return "cerrado";
+    }
+
+    @GetMapping("/cerrado")
+    public String cerrado(Model model){
+        StringBuilder mensaje = new StringBuilder("Cerrado, por favor visítenos desde las ");
+        mensaje.append(apertura);
+        mensaje.append(" hrs. y las ");
+        mensaje.append(cierre);
+        mensaje.append(" hrs. Gracias.");
+
+        model.addAttribute("titulo", "Fuera del horario de atención");
+        model.addAttribute("mensaje", mensaje);
+        return "cerrado";
     }
 }
