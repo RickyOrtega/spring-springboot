553f9abcb953aa05e2d43b16cca59befd54b3e56
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-24 15:51:01 -0500

    Personalizando-error-404-500
    En esta oportunidad estamos personalizando las páginas de error 404 y la 500

--- error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
     @GetMapping("/index")
     public String index(){
+        Integer valor= 100/0;
         return "index";
     }
 }
