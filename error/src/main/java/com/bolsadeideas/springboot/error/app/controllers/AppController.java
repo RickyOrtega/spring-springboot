791c6d24b101a327ee4895f667fff8dbbda0e80e
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-24 13:33:56 -0500

    Manejo-errores
    Creando el proyecto para el manejo de errores

---
+++ error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ error/src/test/java/com/bolsadeideas/springboot/error/app/ErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.error.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
