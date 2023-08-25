b26b9b897096acbd63f9340b054e31e00e5054ce
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-08-25 07:14:16 -0500

    Personalizando-error-aritmetico
    En esta ocasión estamos aprendiendo a controlar y crear páginas del lado del servidor que permitan llevar el manejo de los errores aritméticos

---
+++ error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,21 @@
+package com.bolsadeideas.springboot.error.app.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+
+    @ExceptionHandler(ArithmeticException.class)
+    public String aritmeticaError(Exception ex, Model model){
+        model.addAttribute("error", "Error de aritmética");
+        model.addAttribute("message", ex.getMessage());
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("timestamp", new Date());
+        return "error/aritmetica";
+    }
+}
