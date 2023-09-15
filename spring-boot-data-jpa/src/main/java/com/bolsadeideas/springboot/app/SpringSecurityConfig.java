86451ed2e9d37372609d6d5868933ee8cc0690d3
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-09-14 21:17:18 -0500

    Spring-Security

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -1,28 +1,48 @@
 package com.bolsadeideas.springboot.app;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig{
 
     @Bean
-    public static BCryptPasswordEncoder passwordEncoder(){
+    public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+/*    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER").build());
+
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN", "USER").build());
+
+        return manager;
+    }*/
 
     @Autowired
     public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
         PasswordEncoder encoder = passwordEncoder();
-/*        User.UserBuilder users = User.builder().passwordEncoder(password -> {
-            return encoder.encode(password);
-        });*/
 
         User.UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 
@@ -32,57 +52,38 @@ public class SpringSecurityConfig{
                         .password("12345")
                         .roles("ADMIN", "USER"))
                 .withUser(users
-                        .username("andres")
+                        .username("ricky")
                         .password("12345")
                         .roles("USER"));
 
     }
 
-}
-
-/*
-    @Bean
-    public UserDetailsService userDetailsService()throws Exception{
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User
-                .withUsername("jhon")
-                .password(passwordEncoder().encode("12345"))
-                .roles("USER")
-                .build());
-
-        manager.createUser(User
-                .withUsername("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN","USER")
-                .build());
-
-        return manager;
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> {
-                    try {
-                        authz.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-                                .requestMatchers("/uploads/**").hasAnyRole("USER")
-                                .requestMatchers("/ver/**").hasRole("USER")
-                                .requestMatchers("/factura/**").hasRole("ADMIN")
-                                .requestMatchers("/form/**").hasRole("ADMIN")
-                                .requestMatchers("/eliminar/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                                .and()
-                                .formLogin().permitAll()
-                                .and()
-                                .logout().permitAll();
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+/*                                    .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+                .requestMatchers("/ver/**").hasAnyRole("USER")
+                .requestMatchers("/uploads/**").hasAnyRole("USER")
+                .requestMatchers("/form/**").hasAnyRole("ADMIN")
+                .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                .requestMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();*/
+
+        http.authorizeHttpRequests((authorize) -> { authorize
+                .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+                .requestMatchers("/ver/**").hasAnyRole("USER")
+                .requestMatchers("/uploads/**").hasAnyRole("USER")
+                .requestMatchers("/form/**").hasAnyRole("ADMIN")
+                .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                .requestMatchers("/factura/**").hasAnyRole("ADMIN")
+                .anyRequest().authenticated();
+        }).formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+        .logout(LogoutConfigurer::permitAll);
 
         return http.build();
-
     }
-}*/
+}
