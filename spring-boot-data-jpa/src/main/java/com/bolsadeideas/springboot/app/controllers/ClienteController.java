a5a9031002fa75f07fa47a5df7e49c40de09571b
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-09-29 17:29:53 -0500

    generando-archivo-csv

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -97,7 +97,7 @@ public class ClienteController {
 		return "ver";
 	}
 
-	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	@RequestMapping(value = {"/listar", "/listar/"}, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, Authentication authentication, HttpServletRequest request, Locale locale) {
 
 		if(authentication != null){
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
@@ -1,13 +1,19 @@
 package com.bolsadeideas.springboot.app.view.csv;
 
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import java.util.Map;
 
-@Component("listar.csv")
+@Component("listar")
 public class ClienteCsvView extends AbstractView {
 
 	public ClienteCsvView() {
@@ -23,5 +29,20 @@ public class ClienteCsvView extends AbstractView {
 	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
 		response.setContentType(getContentType());
+
+		Page<Cliente> clientes = (Page<Cliente>) model.get("clientes");
+
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+		String[] header = {"id", "nombre", "apellido", "email", "createAt"};
+		beanWriter.writeHeader(header);
+
+		for(Cliente cliente: clientes){
+			beanWriter.write(cliente, header);
+		}
+
+
+
+		beanWriter.close();
 	}
 }
