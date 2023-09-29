1a4720b3cfb23f1ea76847546fc2bf141689a302
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-09-29 14:49:02 -0500

    añadiendo-datos-factura

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlsxView.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.app.view.xlsx;
 import com.bolsadeideas.springboot.app.models.entity.Factura;
 
 
+import com.bolsadeideas.springboot.app.models.entity.ItemFactura;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.poi.ss.usermodel.*;
@@ -27,7 +28,7 @@ public class FacturaXlsxView extends AbstractXlsxView {
 	@Override
 	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Factura factura = (Factura) model.get("factura");
-		Sheet sheet = workbook.createSheet("Factura Spring");
+		Sheet sheet = workbook.createSheet(messageSource.getMessage("text.factura.ver.datos.factura", null, request.getLocale()));
 
 		Locale locale = localeResolver.resolveLocale(request);
 
@@ -53,9 +54,29 @@ public class FacturaXlsxView extends AbstractXlsxView {
 		cell = row.createCell(0);
 		cell.setCellValue(factura.getCliente().getEmail());
 
-		sheet.createRow(3).createCell(0).setCellValue(mensajes.getMessage("text.factura.ver.datos.factura", locale));
-		sheet.createRow(4).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.folio", locale).concat(": ").concat(factura.getId().toString()));
-		sheet.createRow(5).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.descripcion", locale).concat(": ").concat(factura.getDescripcion()));
-		sheet.createRow(6).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.fecha", locale).concat(": ").concat(factura.getCreatedAt().toString()));
+		sheet.createRow(4).createCell(0).setCellValue(mensajes.getMessage("text.factura.ver.datos.factura", locale));
+		sheet.createRow(5).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.folio", locale).concat(": ").concat(factura.getId().toString()));
+		sheet.createRow(6).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.descripcion", locale).concat(": ").concat(factura.getDescripcion()));
+		sheet.createRow(7).createCell(0).setCellValue(mensajes.getMessage("text.cliente.factura.fecha", locale).concat(": ").concat(factura.getCreatedAt().toString()));
+
+		Row header = sheet.createRow(9);
+		header.createCell(0).setCellValue(mensajes.getMessage("text.factura.form.item.nombre", locale));
+		header.createCell(1).setCellValue(mensajes.getMessage("text.factura.form.item.precio", locale));
+		header.createCell(2).setCellValue(mensajes.getMessage("text.factura.form.item.cantidad", locale));
+		header.createCell(3).setCellValue(mensajes.getMessage("text.factura.form.item.total", locale));
+
+		int rownum = 10;
+
+		for(ItemFactura item: factura.getItems()){
+			Row fila = sheet.createRow(rownum++);
+			fila.createCell(0).setCellValue(item.getProducto().getNombre());
+			fila.createCell(1).setCellValue(item.getProducto().getPrecio());
+			fila.createCell(2).setCellValue(item.getCantidad());
+			fila.createCell(3).setCellValue(item.calcularImporte());
+		}
+
+		Row filatotal = sheet.createRow(rownum);
+		filatotal.createCell(2).setCellValue(mensajes.getMessage("text.factura.form.total", locale));
+		filatotal.createCell(3).setCellValue(factura.getTotal());
 	}
 }
